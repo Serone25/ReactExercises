7b78ad4a5906c6621d8b19383188862e14538f73
Serone25 <fran19842010@live.com>
2022-10-14 18:41:23 +0000

    First Commit

--- .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
+++ .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
@@ -7,12 +7,14 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * 1) Create the mapping function and use it to generate a new array of
  * planets in html called planetsInHTML
  */
-const mappingFunction = (planet, i) => {
-	return <li className="list-group-item">{planet}</li>;
-};
+const mappingFunction = (planet,i) => {};
 const planetsInHTML = planets.map(mappingFunction);
 
 // 2) add the array planetsInHTML inside the innerHTML of this ul
-const content = <ul className="list-group m-5">{planetsInHTML}</ul>;
+const content = (
+	<ul className="list-group m-5">
+		<li className="list-group-item">{planetsInHTML}</li>
+	</ul>
+);
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
--- .learn/resets/04.1-conditional-rendering/app.jsx
+++ .learn/resets/04.1-conditional-rendering/app.jsx
@@ -2,31 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-const Alert = (props) => {
+const Alert = props => {
 	//your component here
-	if (props.color === "red") {
-		return (
-			<div className="alert alert-danger" role="alert">
-				{props.text}
-			</div>
-		);
-	} else if (props.color === "orange") {
-		return (
-			<div className=" alert alert-warning" role="alert">
-				{props.text}
-			</div>
-		);
-	} else if(props.color === "green"){
-		return(
-			<div class="alert alert-success" role="alert">
-				{props.text}
-			</div>
-		)
-	}
+
 };
 Alert.propTypes = {
 	color: PropTypes.string,
-	text: PropTypes.string,
+	text: PropTypes.string
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
@@ -35,7 +17,6 @@ ReactDOM.render(
 	<div>
 		<Alert text="OMG! Something really bad has happended!" color="red" />
 		<Alert text="Well, it is not that bad after all!" color="orange" />
-		<Alert text="I am suppossed to be green" color="green"/>
 	</div>,
 	document.querySelector("#myDiv")
 );
